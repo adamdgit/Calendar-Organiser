b9fd3b59eba58f0f7d137e43d880561a804c025e
adamdgit <adam_d@outlook.com>
2023-01-30 17:53:32 +1100

    Fetch and display all original pokemon on mount

--- app/pokedex/page.tsx
+++ app/pokedex/page.tsx
@@ -1,28 +1,49 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import React, { FormEvent, useRef } from 'react'
-import PokemonList from "./PokemonList"
+import React, { useRef, useEffect, useState } from 'react'
+import { generationList } from '@/types';
+import styles from './styles.module.css'
 
 export default function Pokedex() {
 
+  const [pokemonData, setPokemonData] = useState<generationList>()
+
+  const fetchPokemonData = async () => {
+    const res = await fetch(`https://pokeapi.co/api/v2/generation/1`)
+    const data = await res.json()
+    const orginalPokemon: generationList = data.pokemon_species
+    // sort by ascending order 1-151
+    setPokemonData(orginalPokemon.sort((a,b) => {
+        return a.url.slice(a.url.length-4, a.url.length).match(/\d+/g) - b.url.slice(b.url.length-5, b.url.length).match(/\d+/g)
+      })
+    )
+  }
+
+  useEffect(() => {
+    fetchPokemonData()
+  }, [])
+  
   const router = useRouter();
   const searchInput = useRef();
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    router.push(`/pokedex/${searchInput.current.value}`)
-  }
 
   return (
     <div>
       <h2>Pokedex</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" ref={searchInput} />
-        <button type="submit">Search by ID</button>
-      </form>
+      <input type="text" ref={searchInput} />
+      <button type="submit">Search by ID</button>
 
-      <PokemonList />
+      <div className={styles.namelist}>
+        {
+          pokemonData?.map((pokemon, i) => (
+            <a key={i} 
+              onClick={() => router.push(`/pokedex/${i+1}`)}
+              className={styles.name}>
+              {i+1}: {pokemon.name}
+            </a>
+          ))
+        }
+      </div>
     </div>
   )
 }
