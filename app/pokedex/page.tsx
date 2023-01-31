6810d0593ecba91894dd11fb158f2b5711842131
adamdgit <adam_d@outlook.com>
2023-01-31 17:52:21 +1100

    Added correct pokedex nums to dataset

--- app/pokedex/page.tsx
+++ app/pokedex/page.tsx
@@ -1,45 +1,70 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { generationList } from '@/types';
 import styles from './styles.module.css'
 
+type sortedPokemonProps = [{
+  dexNum: number,
+  name: string,
+  url: string
+}]
+
 export default function Pokedex() {
 
-  const [pokemonData, setPokemonData] = useState<generationList>()
+  const [pokemonData, setPokemonData] = useState<sortedPokemonProps | undefined>()
+  const [filteredData, setFilteredData] = useState<sortedPokemonProps | undefined>()
+
+  const router = useRouter();
 
   const fetchPokemonData = async () => {
     const res = await fetch(`https://pokeapi.co/api/v2/generation/1`)
     const data = await res.json()
-    const orginalPokemon: generationList = data.pokemon_species
-    // sort by ascending order 1-151
-    setPokemonData(orginalPokemon.sort((a,b) => {
-        return a.url.slice(a.url.length-4, a.url.length).match(/\d+/g) - b.url.slice(b.url.length-5, b.url.length).match(/\d+/g)
-      })
-    )
+    // sort by ascending order 1-151 as given order from API is incorrect
+    // need to slice the last part of the url string and check for digits to get number
+    const sortedPokemon: generationList = data.pokemon_species.sort((a,b) => {
+      return a.url.slice(a.url.length-4, a.url.length).match(/\d+/g) - b.url.slice(b.url.length-5, b.url.length).match(/\d+/g)
+    })
+    // adding indexs to use for search function
+    const fixedPokemon:sortedPokemonProps = sortedPokemon.map((pokemon, index) => (
+      {...pokemon, dexNum: index+1}
+    ))
+    console.log(fixedPokemon)
+    setPokemonData(fixedPokemon)
+  }
+
+  const handleSearch = (search:string) => {
+    setFilteredData(pokemonData?.filter(a => {
+      if (a.name.includes(search)) return a
+    }))
   }
 
   useEffect(() => {
     fetchPokemonData()
   }, [])
-  
-  const router = useRouter();
-  const searchInput = useRef();
 
   return (
     <div>
       <h2>Pokedex</h2>
-      <input type="text" ref={searchInput} />
-      <button type="submit">Search by ID</button>
+      <input type="text" onChange={(e) => handleSearch(e.target.value)} />
 
       <div className={styles.namelist}>
         {
+          filteredData ? 
+          filteredData?.map((pokemon, i) => (
+            <a key={i} 
+              onClick={() => router.push(`/pokedex/${pokemon.dexNum}`)}
+              className={styles.name}>
+              {pokemon.dexNum}: {pokemon.name}
+            </a>
+          ))
+          :
           pokemonData?.map((pokemon, i) => (
             <a key={i} 
-              onClick={() => router.push(`/pokedex/${i+1}`)}
+              onClick={() => router.push(`/pokedex/${pokemon.dexNum}`)}
               className={styles.name}>
-              {i+1}: {pokemon.name}
+              {pokemon.dexNum}: {pokemon.name}
             </a>
           ))
         }
