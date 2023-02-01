d1c197f84c02ca8967f2d9c0b1c51336f079d2b3
adamdgit <adam_d@outlook.com>
2023-02-01 14:36:06 +1100

    Added stats and shiny sprite

--- app/pokedex/GetPokemonById.tsx
+++ app/pokedex/GetPokemonById.tsx
@@ -6,24 +6,43 @@ import { PokemonDataProps } from '@/types';
 export default function GetPokemonById({ data } : { data: PokemonDataProps }) {
 
   return (
-    <div>
-      <h2>{data?.name.toUpperCase()}</h2>
-        {
-          data && 
-          <Image 
-            key={data.id}
-            src={data?.sprite} 
-            alt={data?.name + "sprite"} 
-            width={96} height={96}
-          />
-        }
-        <ul className={styles.moveList}>
-          {
-            data?.moves.map((move, i) => (
-              <li key={i}>{move.move.name}</li>
-            ))
-          }
-        </ul>
-    </div>
+    <main className={styles.mainContent}>
+      {
+        data && 
+        <div className={styles.pokemonInfo}>
+          <h2 style={{textAlign: 'center'}}>{data?.name.toUpperCase()}</h2>
+          <div className={styles.spritesContainer}>
+            <Image 
+              key={data.id}
+              src={data?.sprite} 
+              alt={data?.name + "sprite"} 
+              width={96} height={96}
+            />
+            <Image 
+              key={data.id + 'shiny'}
+              src={data?.shiny} 
+              alt={data?.name + "shiny sprite"} 
+              width={96} height={96}
+            />
+          </div>
+          <span className={styles.heading3}>Stats</span>
+          <div className={styles.stats}>
+            {
+              data?.stats.map((stat, i) => {
+                return <span key={i}>{stat.stat.name + ': ' + stat.base_stat}</span>
+              })
+            }
+          </div>
+          <span className={styles.heading3}>Moves</span>
+          <ul className={styles.moveList}>
+            {
+              data?.moves.map((move, i) => (
+                <li key={i}>{move.move.name}</li>
+              ))
+            }
+          </ul>
+        </div>
+      }
+    </main>
   )
 }
