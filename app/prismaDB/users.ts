a9279a6223512178e195927300875367112264d9
adamdgit <adam_d@outlook.com>
2023-02-05 19:06:15 +1100

    Testing creating and searching db for data

--- app/prismaDB/users.ts
+++ app/prismaDB/users.ts
@@ -2,18 +2,30 @@ import prisma from ".";
 
 export async function getUser(email:string) {
   try {
-    const user = await prisma.user.findUnique({ where: {email: email} })
+    const user = await prisma.user.findUnique({ 
+      where: { 
+        email: email 
+      }
+    })
     return { user }
   } catch (error) {
     return { error }
   }
 }
 
-export async function createUser(email:string) {
+export async function createUser(email: string, name: string) {
   try {
-    const user = await prisma.user.create({ data: { email:email }, select: { id: true } })
+    const user = await prisma.user.create({ 
+      data: { 
+        email: email,
+        name: name
+      }, 
+      select: { id: true } 
+    })
+    console.log(user)
     return { user }
-  } catch (error) {
+  } catch ( error ) {
     return { error }
   }
+
 }
