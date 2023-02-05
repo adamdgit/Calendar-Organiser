011c3d06fd8e85177a7e874d2810e585bac2647e
adamdgit <adam_d@outlook.com>
2023-02-06 02:57:19 +1100

    Display a users team of pokemon

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import { getUser } from "./prismaDB/users"
+import { getUser, getTeam } from "./prismaDB/users"
 
 export default async function Home() {
-  const { user } = await getUser('adam_d@outlook.com')
+  
+
+    const { user } = await getUser('adam_d@outlook.com')
+    const { team } = await getTeam(user?.id)
+    
   
   return (
     <div>
       <h1>Your Team</h1>
       {
-        user && <p>{ user?.id }</p>
+        user && <p>{ `User: ${user?.id} ${user?.name}` }</p>
+      }
+      {
+        team && <div>
+          <p>{team.name}</p>
+          {team.pokemon.map(pokemon => (
+            pokemon.name
+          ))}
+        </div>
       }
     </div>
   )
