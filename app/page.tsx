bbb59ef1a6f49285c58769658c44d03c2e10eaf4
adamdgit <adam_d@outlook.com>
2023-02-15 15:13:10 +1100

    Creating team component

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,18 @@
 import React from 'react'
 import { useSession } from 'next-auth/react'
 import styles from './styles.module.css'
+import PokemonTeam from './components/pokemonTeam';
 
 export default function Home() {
-  
+
   const session = useSession();
   console.log(session)
 
   if (session.status === 'authenticated') {
     return (
       <div>
-        <h2>Hello {session.data.user?.name}</h2>
+        <h2>Your Pokemon Team</h2>
+        <PokemonTeam />
       </div> 
     )
   } else {
