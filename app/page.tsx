d46dffc05f2d39a0aaf76120ae78bf23abac38d4
adamdgit <adam_d@outlook.com>
2023-03-14 16:48:23 +1100

    session check at top level, display app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import App from "./components/App"
-import { getUserEvents } from "./prismaDB/users"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getUserEvents } from "./prismaDB/users";
+
 export default async function Home() {
 
-  const { events } = await getUserEvents('adammdemol@gmail.com')
-  console.log(events[0].calendarEvent)
+  const session = await getServerSession(authOptions)
+  const { events } = await getUserEvents(session.email)
 
-  return (
-    <App calendarEvents={events[0].calendarEvent}/>
-  )
+  if (session) {
+    return <App events={events[0].calendarEvent} />
+  } else {
+    return <div>Please login to view your calendar.</div>
+  }
+
 }
