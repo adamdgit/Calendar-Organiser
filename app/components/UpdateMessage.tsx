9fffeedf989b99fad9c2139061bb0da3a0f5cc02
adamdgit <adam_d@outlook.com>
2023-03-19 13:19:56 +1100

    Added component for updating messages to user

---
+++ app/components/UpdateMessage.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import styles from "../styles.module.css"
+
+export default function UpdateMessage(props: 
+  {message: string, needsUpdate: boolean, setNeedsUpdate: (args: boolean) => void}) {
+
+  useEffect(() => {
+    setTimeout(() => {
+      props.setNeedsUpdate(false)
+    }, 2000)
+  }, [props.needsUpdate])
+
+  return (
+    <div 
+      className={styles.updateMessage} 
+      style={props.needsUpdate ? {opacity: '1', bottom: '20px'} : {}}>
+      {props.message}
+    </div>
+  )
+}
