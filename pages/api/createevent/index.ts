08907ecf03d5dff55501dbd94bfe76054fabaa0b
adamdgit <adam_d@outlook.com>
2023-03-22 17:51:32 +1100

    Get updated events after creating a new one

--- pages/api/createevent/index.ts
+++ pages/api/createevent/index.ts
@@ -1,9 +1,11 @@
-import { createEvent } from "@/app/prismaDB/users"
+import { createEvent, getUserEvents } from "@/app/prismaDB/users"
 
 export default async function handler(req: Request, res: Response) {
+
   if (req.method !== "POST") return Error("Wrong request method") 
-  console.log(req.body)
     const data = req.body
     const { errorMsg } = await createEvent(data.email, data.date, data.description)
-    if (!errorMsg) res.status(200).json("Event created successfully")
+    const events = await getUserEvents(data.email)
+    if (!errorMsg) res.status(200).json(events)
+
 }
