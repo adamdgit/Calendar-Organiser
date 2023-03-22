6b542b3a4f338b01aad289d3c8c98a1fd67e6a88
adamdgit <adam_d@outlook.com>
2023-03-22 17:51:48 +1100

    Get updated events after deleting one

--- pages/api/deleteEvent/index.ts
+++ pages/api/deleteEvent/index.ts
@@ -1,8 +1,14 @@
-import { deleteEvent } from "@/app/prismaDB/users"
+import { deleteEvent, getUserEvents } from "@/app/prismaDB/users"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handler(req: Request, res: Response) {
+
+  const session = getServerSession(authOptions)
+
   if (req.method !== "POST") return Error("Wrong request method") 
     const data = req.body
     const { errorMsg } = await deleteEvent(data.id)
-    if (!errorMsg) res.status(200).json("Event created successfully")
+    const events = await getUserEvents(session.email)
+    if (!errorMsg) res.status(200).json(events)
 }
