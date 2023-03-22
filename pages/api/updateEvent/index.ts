e6c26b01cdf8ec6457dd35715c6aa29954e34762
adamdgit <adam_d@outlook.com>
2023-03-23 00:39:56 +1100

    Return updated events when editing event

--- pages/api/updateEvent/index.ts
+++ pages/api/updateEvent/index.ts
@@ -1,8 +1,15 @@
-import { updateEvent } from "@/app/prismaDB/users"
+import { getUserEvents, updateEvent } from "@/app/prismaDB/users"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handler(req: Request, res: Response) {
+
+  const session = getServerSession(authOptions)
+
   if (req.method !== "POST") return Error("Wrong request method") 
     const data = req.body
     const { errorMsg } = await updateEvent(data.id, data.description)
-    if (!errorMsg) res.status(200).json("Event updated successfully")
+    const events = await getUserEvents(session.email)
+    if (!errorMsg) res.status(200).json(events)
+
 }
