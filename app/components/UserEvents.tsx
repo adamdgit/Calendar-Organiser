451a4c448bc568d6096131d312774812c1ca9dff
adamdgit <adam_d@outlook.com>
2023-03-24 01:08:55 +1100

    WIP: temporary solution

--- app/components/UserEvents.tsx
+++ app/components/UserEvents.tsx
@@ -13,8 +13,11 @@ type lsItemsProps = {
 export default function UserEvents({ eventItems, setEventItems } : lsItemsProps) {
   
   const [months, setMonths] = useState<string[]>([])
+  const [years, setYears] = useState<string[]>([])
 
   useEffect(() => {
+
+    console.log(eventItems)
 
     // Store months which have events to show/hide related events
     let tempMonths:string[] = [];
@@ -24,25 +27,42 @@ export default function UserEvents({ eventItems, setEventItems } : lsItemsProps)
     // remove duplicates using set and spread operator
     setMonths([...new Set(tempMonths)]);
 
+    // Store months which have events to show/hide related events
+    let tempYears:string[] = [];
+    eventItems.map(item => {
+      tempYears.push(new Date(item.date).toLocaleString('en-au', {year: 'numeric'}))
+    });
+    // remove duplicates using set and spread operator
+    setYears([...new Set(tempYears)]);
+
   }, [eventItems])
+
+
 
   return (
     <div className={styles.lsItemsWrap}>
     {
-      months.map(month => (
-        <div key={month} className={styles.monthEventWrap}>
-          <h3>{month}</h3>
+      years.map(year => (
+        <div key={year} className={styles.yearEventWrap}>
+          <h3>{year}</h3>
           {
-            eventItems?.sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
-            .map((item, i) => 
-              new Date(item.date).toLocaleString('en-au', {month: 'long'}) === month ? 
-                <EventItem 
-                  key={i} 
-                  item={item} 
-                  setEventItems={setEventItems} 
-                />
-              : null
-            )
+          months.map(month => 
+            <div key={month} className={styles.monthEventWrap}>
+              <h4>{month}</h4>
+              {
+                eventItems?.sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
+                .map((item, i) => 
+                  new Date(item.date).toLocaleString('en-au', {month: 'long'}) === month && new Date(item.date).toLocaleString('en-au', {year: 'numeric'}) === year ? 
+                    <EventItem 
+                      key={i} 
+                      item={item} 
+                      setEventItems={setEventItems} 
+                    />
+                  : null
+                )
+              }
+            </div>
+          )
           }
         </div>
       ))
