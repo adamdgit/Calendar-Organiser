7342472bdfc0e0551730bd68379a481b4007075a
adamdgit <adam_d@outlook.com>
2023-03-24 13:28:16 +1100

    UseMemo for expensive calcs, sort months

--- app/components/UserEvents.tsx
+++ app/components/UserEvents.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import styles from '../styles.module.css'
 import { calendarEventProps } from "./App";
 import EventItem from "./EventItem";
@@ -15,7 +15,7 @@ export default function UserEvents({ eventItems, setEventItems } : lsItemsProps)
   const [months, setMonths] = useState<string[]>([]);
   const [years, setYears] = useState<string[]>([]);
 
-  useEffect(() => {
+  useMemo(() => {
 
     let tempMonths:string[] = [];
     eventItems.map(item => {
@@ -40,7 +40,7 @@ export default function UserEvents({ eventItems, setEventItems } : lsItemsProps)
         <div key={year} className={styles.yearEventWrap}>
           <h3>{year}</h3>
           {
-          months.map(month => 
+          months?.sort((a, b) => Date.parse(a) - Date.parse(b)).map(month => 
             new Date(month).toLocaleString('en-au', {year: 'numeric'}) === year ? 
             <div key={month} className={styles.monthEventWrap}>
               <h4>{month}</h4>
